cc4d8408526f20909052c7af8f62bde2bea2f5c6
Kojoel <nanaabruquah1s@gmail.com>
2024-09-02 11:36:10 +0000

    Creating and subscribing to an observable using 'of'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Exploring-Basic-RxJs-Observables';
+
+  constructor(){
+    // Creating and Subscribing to an Observable with of:
+    const numbers$ = of([1,2,3,4,5]);
+    numbers$.subscribe(number => console.log("Using of", number));
+
+    
+  }
 }
